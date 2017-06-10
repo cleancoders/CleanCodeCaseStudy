2d0b2f5aa68c8e0ac4d0c3904925ed0b46c1334b
Alexandros Antoniadis <antoniadis.alexandros.87@gmail.com>
2017-06-10 17:55:09 +0300

    adds maven-compiler-plugin inclusion in project.clj

--- project.clj
+++ project.clj
@@ -5,6 +5,8 @@
                  [org.fitnesse/fitnesse "20140630"]
                  [de.bechte.junit/junit-hierarchicalcontextrunner "4.11.3"]]
 
+  :pom-plugins [[org.apache.maven.plugins/maven-compiler-plugin "3.6.1" {:configuration ([source "1.8"] [target "1.8"])} ]]
+
   :repositories [["sonatype" {:url "http://oss.sonatype.org/content/repositories/releases"}]]
 
   :aliases {"fitnesse" ["run" "-m" "fitnesseMain.FitNesseMain" "-p" "8080"]}
